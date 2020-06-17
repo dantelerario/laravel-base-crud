94c4e5a86d3531dee3ea3891e74fd0876c302e40
Dante L'Erario <dantelerario@gmail.com>
2020-06-17 16:54:01 +0200

    model

--- database/migrations/2020_06_16_091302_create_students_table.php
+++ database/migrations/2020_06_16_091302_create_students_table.php
@@ -16,7 +16,9 @@ class CreateStudentsTable extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('gender', 1);
             $table->text('description');
+            $table->tinyInteger('age');
             $table->timestamps();
         });
     }
